9547eb6d75bd1776e87b75d7d0b95ca138f83c99
ElenaPrudnikova <elena8sen@yandex.ru>
2022-04-17 15:39:50 +0300

    add task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA > numberB)
+{
+    Console.WriteLine("Наибольшее число: " + numberA);
+    Console.WriteLine("Наименьшее число: " + numberB);
+}
+
+if(numberB > numberA)
+{
+    Console.WriteLine("Наибольшее число: " + numberB);
+    Console.WriteLine("Наименьшее число: " + numberA);
+}
