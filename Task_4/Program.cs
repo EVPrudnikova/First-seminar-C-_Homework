0abbe77a799fb9ddda48b547d010956b1dfb94be
ElenaPrudnikova <elena8sen@yandex.ru>
2022-04-17 17:19:53 +0300

    add task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int Max = numberA;
+
+if (numberB > Max) Max = numberB;
+if (numberC > Max) Max = numberC;
+
+Console.WriteLine("Максимальное число: " + Max);
