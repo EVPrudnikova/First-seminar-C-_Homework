1bf97a670c9e2584de2f829b2d62014403b7c161
ElenaPrudnikova <elena8sen@yandex.ru>
2022-04-17 18:06:36 +0300

    add task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Index = 1;
+
+while (Index < number)
+{
+    if (Index % 2 == 0)
+    {
+        Console.Write(Index + ", ");
+    }
+Index++;
+}
