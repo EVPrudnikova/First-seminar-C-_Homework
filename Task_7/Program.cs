2c817391dc1cab5cff2c8c1780be73b1ceab276d
ElenaPrudnikova <elena8sen@yandex.ru>
2022-04-17 15:27:32 +0300

    add task_7

---
+++ Task_7/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = number % 10;
+
+if(a >= 100||a <=999)
+{
+    Console.WriteLine("Последняя цифра: " + a);
+}
